3cdf7fc3c011a77178f207e99bfa7608cff13636
Jean Moschen - Pessoal <jeanfelipe0610@gmail.com>
2023-08-08 20:11:32 -0300

    init

---
+++ src/main/java/com/uniamerica/estatisticas/EstatisticasApplication.java
@@ -0,0 +1,13 @@
+package com.uniamerica.estatisticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatisticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatisticasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniamerica/estatisticas/EstatisticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniamerica.estatisticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstatisticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
